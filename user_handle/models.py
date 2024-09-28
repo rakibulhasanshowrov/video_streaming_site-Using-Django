bef95584ab6a61be7ffc56d8a0ad765c9446f588
rakibulhasanshowrov <rakibulhasanshowrov@outlook.com>
2024-09-28 23:54:38 +0600

    base.html

---
+++ user_handle/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class UserProfile(models.Model):
+  user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='user_profile')
+  dob=models.DateField(blank=True,null=True)
+  profile_pic=models.ImageField(upload_to='profile_pics',blank=True)
+  type=[
+    ('teacher','Teacher'),
+    ('student','Student'),
+    ('Genaral_user','General User'), 
+  ]
+  profile_type=models.CharField(blank=False,choices=type,default="General User")
+  
+  
+  def __str__(self):
+    return self.user.username
+  
---
+++ user_handle/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from user_handle import views
+
+urlpatterns=[
+  path('',views.homepage,name='homepage'),
+  path('signup/',views.sign_up,name='signup'),
+  path('login/',views.login_user ,name='login'),
+  # path('logout/',views.logout_user ,name='logout'),
+  # path('profile/',views.user_profile ,name='profile'),
+  
+]
